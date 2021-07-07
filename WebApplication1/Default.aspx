303935725194f15a1cdbda965695cbd4ec6297d5
sduBurak <51060856+sduBurak@users.noreply.github.com>
2021-07-07 21:50:54 +0300

    Add files via upload

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %> 
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+
+    </div>
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.App_Code;
+using WebApplication1.App_Code.dsFirmaTableAdapters;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dsFirma.FirmaDataTable firma = new dsFirma.FirmaDataTable();
+
+            firma.NewFirmaRow();
+
+            FirmaTableAdapter.
+
+        }
+    }
+}
